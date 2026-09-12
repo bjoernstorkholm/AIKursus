d73ba177d3f3348a53c60cf49c27d8193d9a22b1
bjoernstorkholm <bjoern@storkholm.dk>
2026-09-12 16:03:52 +0200

    Add test menu button with Lorem ipsum popup

--- ProjektRadar/wwwroot/js/projektradar.js
+++ ProjektRadar/wwwroot/js/projektradar.js
@@ -79,6 +79,11 @@ window.projectRadar = (() => {
         document.getElementById(elementId)?.scrollIntoView({ behavior: "smooth", block: "start" });
     };
 
+    const showDialog = (elementId) => {
+        const dialog = document.getElementById(elementId);
+        if (dialog && !dialog.open) dialog.showModal();
+    };
+
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     };
@@ -110,6 +115,7 @@ window.projectRadar = (() => {
         initializeDragDrop,
         downloadBase64,
         scrollTo,
+        showDialog,
         scrollToTop,
         initToTop
     };
